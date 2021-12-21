320e4787eb74cd64fd43466c6e195a84637f04bc
Ladadori <vladagolunova@gmail.com>
2021-12-21 17:35:21 +1000

    Add ackermann-function

---
+++ level 0/ackermann.js
@@ -0,0 +1,13 @@
+const ackermann = (m, n) => {
+    if (m === 0) {
+      return n + 1;
+    }
+    if (n === 0) {
+      return ackermann(m - 1, 1);
+    }
+    return ackermann(m - 1, ackermann(m, n - 1));
+  };
+  
+  export default ackermann;
+
+  //ура, решение как у учителя!
