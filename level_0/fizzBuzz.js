4ef4ede903ffd348367d3752c8538893497c131b
Ladadori <vladagolunova@gmail.com>
2021-12-23 15:32:59 +1000

    Add update

--- level_0/fizzBuzz.js
+++ level_0/fizzBuzz.js
@@ -4,21 +4,16 @@
 //В остальных случаях выводится само число
 
 export default (begin, end) => {
-    if (begin > end) return;
-    for (let i = begin; i <= end; i += 1) {
-      if (i % 5 === 0 && i % 3 === 0) {
-        console.log('FizzBuzz');
-        continue;
-      }
-      if (i % 3 === 0) {
-        console.log('Fizz');
-        continue;
-      }
-      if (i % 5 === 0) {
-        console.log('Buzz');
-        continue;
-      };
+  if (begin > end) return;
+  for (let i = begin; i <= end; i += 1) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
       console.log(i);
-    };
-    return;
-  };
+    }
+  }
+};
